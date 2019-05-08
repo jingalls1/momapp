d9c9031ebeb65619c8b513eb440eb1892f8e680c
Jacob Ingalls <jingalls@uoregon.edu>
2019-05-07 23:14:45 -0700

    Updates

---
+++ precache-manifest.9fe30a955b5bd08358d4a750e0ff42c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869c96b1b29fbb52019e5c4f07001e50",
+    "url": "/momapp/index.html"
+  },
+  {
+    "revision": "f24bf11018870e795aa6",
+    "url": "/momapp/static/css/main.9702442e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/momapp/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f24bf11018870e795aa6",
+    "url": "/momapp/static/js/main.722111ec.chunk.js"
+  },
+  {
+    "revision": "f584cec6208dea9a846a",
+    "url": "/momapp/static/js/runtime~main.7c8f65b2.js"
+  }
+]);
